ae49e7223ca6ded1193c04387707ab5149ac0797
raisa <RAISA.22@ichat.sp.edu.sg>
2023-10-30 15:32:48 +0800

    changes due to code change in bmi

--- Test_bmi.py
+++ Test_bmi.py
@@ -3,12 +3,18 @@ def test_bmi_normal_weight():
     input_value_weight = 54
     input_value_height = 1.68
     test_result = 0
-    result = bmi.calculate_bmi(input_value_height, input_value_weight)
+    result = bmi.calculate_bmi(input_value_weight, input_value_height)
     assert (result == test_result)
 def test_bmi_over_weight():
-    input_value_weight = 54
+    input_value_weight = 100
     input_value_height = 1.68
+    test_result = 1
     result = bmi.calculate_bmi(input_value_height, input_value_weight)
-
+    assert (result == test_result)
 
 def test_bmi_under_weight():
+    input_value_weight = 5
+    input_value_height = 1.68
+    test_result = -1
+    result = bmi.calculate_bmi(input_value_height, input_value_weight)
+    assert (result == test_result)
